b31552c52dfe2cc450361a52a7c218f67cf72257
vladislav2251 <vladdidyk1985@gmail.com>
2025-02-24 17:10:53 +0200

    feat: added switch icons functional for appearance

--- modules/dashboard/components/switch-theme.vue
+++ modules/dashboard/components/switch-theme.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-import { IconMoon } from '#components';
 import { useColorMode } from '#imports';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { MoonIcon, SunIcon } from 'lucide-vue-next';
 
 const colorMode = useColorMode();
-
 function setTheme(mode: 'system' | 'light' | 'dark') {
     colorMode.preference = mode;
 }
+
+const themeIcon = computed(() => {
+    if (colorMode.preference === 'system') {
+        return colorMode.value === 'dark' ? MoonIcon : SunIcon;
+    }
+    return colorMode.preference === 'dark' ? MoonIcon : SunIcon;
+});
 </script>
 
 <template>
     <DropdownMenu>
         <DropdownMenuTrigger>
             <div :class="cn(buttonVariants({ variant: 'outline', size: 'icon' }))">
-                <IconMoon />
+                <component :is="themeIcon" />
             </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent class="min-w-32" align="end">
